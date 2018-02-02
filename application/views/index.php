bb30f6c546a26a741b41f213500323de75906628
mkashif11 <mohdkashif11@gmail.com>
2018-02-03 01:21:51 +0530

    login page

--- application/views/index.php
+++ application/views/index.php
@@ -1,22 +1,24 @@
-<div class="loginbox">
-	<div class="loginHeader">
-		AdvMed - Login 
-	</div>
-	<div class="loginBody">
-		<div class="loginUser">
-			<span class="label">Username:</span>
-			<span class="lableInput">
-				<input type="text" name="userName" id="user" class="usewrInptbx">
-			</span>
+<div class="login-bg">
+	<div class="loginbox">
+		<div class="loginHeader">
+			Login - Star Services  
 		</div>
-		<div class="loginUser">
-			<span class="label">Password:</span>
-			<span class="lableInput">
-				<input type="passwrod" name="userName" id="passwd" class="usewrInptbx">
-			</span>
+		<div class="loginBody">
+			<div class="loginUser">
+				<span class="label">Username:</span>
+				<span class="lableInput">
+					<input type="text" name="userName" id="user" class="usewrInptbx">
+				</span>
+			</div>
+			<div class="loginUser">
+				<span class="label">Password:</span>
+				<span class="lableInput">
+					<input type="passwrod" name="userName" id="passwd" class="usewrInptbx">
+				</span>
+			</div>
 		</div>
-	</div>
-	<div class="logibBtnrw">
-		<input type="button" value="SignIn" class="loginBtn" id="login">
+		<div class="logibBtnrw">
+			<input type="button" value="SignIn" class="loginBtn" id="login">
+		</div>
 	</div>
 </div>
