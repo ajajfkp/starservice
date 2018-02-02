42d004e1126d28d28e7c16403b04e6a60726d666
mkashif11 <mohdkashif11@gmail.com>
2018-02-03 00:49:14 +0530

    New Add popup

--- application/views/home/addpopup.php
+++ application/views/home/addpopup.php
@@ -130,3 +130,161 @@
 		</div>
 	</div>
 </div>
+
+
+<!---------------------------Add New Deatil popup Code Start here--------------------------->
+
+
+	<div id="addNewEntry" class="add-new-deatil-main-con">
+		<div class="popu-header">
+			<span class="heading">Add New Entry:</span>
+			<span id="popupClose" class="popup-Close">X</span>
+		</div>
+		<div class="popup-body left">
+			<div class="popup-body-inr-hdng">
+				Prodcut Detail
+			</div>
+			<div class="popup-body-inner-rw-lt">
+				<div class="popup-body-inner-rw">
+					<span class="inner-rw-label">Product:</span>
+					<span class="inner-rw-val">
+						<select class="popup-slectBx">
+							<option value=""></option>
+							<option value="">Battery</option>
+							<option value="">Inverter</option>
+							<option value="">Car battery</option>
+						</select>
+					</span>
+				</div>
+				<div class="popup-body-inner-rw">
+					<span class="inner-rw-label">Brand:</span>
+					<span class="inner-rw-val">
+						<select class="popup-slectBx">
+							<option value=""></option>
+							<option value="">Mickrotek</option>
+							<option value="">Luminous</option>
+							<option value="">Okaya</option>
+						</select>
+					</span>
+				</div>
+				<div class="popup-body-inner-rw">
+					<span class="inner-rw-label">Model No:</span>
+					<span class="inner-rw-val">
+						<input type="text" name="modelNum" class="popup-inpt">
+					</span>
+				</div>
+				<div class="popup-body-inner-rw">
+					<span class="inner-rw-label">Warranty:</span>
+					<span class="inner-rw-val">
+						<select class="popup-slectBx">
+							<option value=""><option>
+							<option value="">3 Months</option>
+							<option value="">6 Months</option>
+							<option value="">12 Months</option>
+						</select>
+					</span>
+				</div>
+			</div>
+			<div class="popup-body-inner-rw-rt">
+				<div class="popup-body-inner-rw">
+					<span class="inner-rw-label">Sell Date:</span>
+					<span class="inner-rw-val">
+						<input type="text" name="dateSold" class="popup-inpt">
+					</span>
+				</div>
+				<div class="popup-body-inner-rw">
+					<span class="inner-rw-label">Services:</span>
+					<span class="inner-rw-val">
+						<select class="popup-slectBx">
+							<option value=""><option>
+							<option value="">1</option>
+							<option value="">2</option>
+							<option value="">3</option>
+						</select>
+					</span>
+				</div>
+				<div class="popup-body-inner-rw">
+					<span class="inner-rw-label">Duration:</span>
+					<span class="inner-rw-val">
+						<select class="popup-slectBx">
+							<option value=""><option>
+							<option value="">15 days</option>
+							<option value="">30 days</option>
+							<option value="">60 days</option>
+							<option value="">90 days</option>
+						</select>
+					</span>
+				</div>
+				<div class="popup-body-inner-rw">
+					<span class="inner-rw-label">Warranty End:</span>
+					<span class="inner-rw-val">
+						<input type="text" name="dateSold" class="popup-inpt">
+					</span>
+				</div>
+			</div>
+			<div class="popup-body-inr-hdng">
+				Customer Detail
+			</div>
+			<div class="popup-body-inner-rw-lt">
+				<div class="popup-body-inner-rw">
+					<span class="inner-rw-label">Name:</span>
+					<span class="inner-rw-val">
+						<input type="text" name="dateSold" class="popup-inpt">
+					</span>
+				</div>
+				<div class="popup-body-inner-rw">
+					<span class="inner-rw-label">Address:</span>
+					<span class="inner-rw-val">
+						<input type="text" name="dateSold" class="popup-inpt">
+					</span>
+				</div>
+			</div>
+			<div class="popup-body-inner-rw-rt">
+				<div class="popup-body-inner-rw">
+					<span class="inner-rw-label">Mobile #:</span>
+					<span class="inner-rw-val">
+						<input type="text" name="dateSold" class="popup-inpt">
+					</span>
+				</div>
+				<div class="popup-body-inner-rw">
+					<span class="inner-rw-label">Note:</span>
+					<span class="inner-rw-val">
+						<textarea class="popup-txtarea"></textarea>
+					</span>
+				</div>
+			</div>
+			<div class="popup-body-inr-hdng">
+				Additional Detail
+			</div>
+			<div class="popup-body-inner-rw-lt">
+				<div class="popup-body-inner-rw">
+					<span class="inner-rw-label">Referral:</span>
+					<span class="inner-rw-val">
+						<select class="popup-slectBx">
+							<option value=""><option>
+							<option value="">Whatsapp</option>
+							<option value="">Facebook</option>
+							<option value="">Just Dial</option>
+							<option value="">Friends</option>
+						</select>
+					</span>
+				</div>
+			</div>
+			<div class="popup-body-inner-rw-rt">
+				<div class="popup-body-inner-rw">
+					<span class="inner-rw-label">Other:</span>
+					<span class="inner-rw-val">
+						<textarea class="popup-txtarea"></textarea>
+					</span>
+				</div>
+			</div>
+		</div>
+		<div class="popup-body-inner-rw">
+			<span class="btn-cancel right">Cancel</span>
+			<span class="btn-save right">Save</span>
+		</div>
+	</div>
+
+
+<!---------------------------Add New Deatil popup Code End here--------------------------->
+
